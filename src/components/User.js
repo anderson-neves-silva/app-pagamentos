92b5c56e14e913f082c627a8e64b0572a8f2deee
Anderson Neves Da Silva <andersonneves_@hotmail.com>
2021-05-13 16:43:59 -0300

    Finalização da consulta com resposta da API por meio do método POST

--- src/components/User.js
+++ src/components/User.js
@@ -1,71 +1,144 @@
 import React from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 import '../styles/global.css';
 
 function User() {
-  // Criando uma variável e a função que irá alterar o valor dela através do hook, e ela começa valendo um array vazio
-  const [persons, setPersons] = useState([]);
+  // A setPersons irá alterar o valor de persons através do hook, Obs. começa valendo um array vazio
+  const [persons, setPersons ]= useState([]);
   // Criando a variável na state que irá mostrar o modal que inicia valendo none
-  const [mostraModal, setMostraModal] = useState("none"); 
+  const [viewModal, setViewModal] = useState("none");  
+  // Objeto de valor inicial vazio
+  const [selectPerson, setSelectPerson] = useState({});
+  // Iniciando com valor vazio, Obs. variável que trata o input para envio
+  const [searchParam1, setSearchParam1] = useState("");
+  // 
+  const [searchParam2, setSearchParam2] = useState("");
+ 
+  let cards = [    
+    { // valid card
+      card_number: '1111111111111111',
+      cvv: 789,
+      expiry_date: '01/18',
+    },    
+    { // invalid card
+      card_number: '4111111111111234',
+      cvv: 123,
+      expiry_date: '01/20',
+    },
+  ];
 
-  // Chamando a API, Obs essa chamada devolve uma promise
-  fetch("https://www.mocky.io/v2/5d531c4f2e0000620081ddce")
-  .then((data) => data.json())  // Retorna um dado qualquer e transforma em json
-  .then((result) => { // Retornando o resultado
-    // console.log(result);
-    setPersons(result);
-  })
+  // Chamando a API, ela fa exibir todas as pessoas na tela, Obs essa chamada devolve uma promise
+  useEffect(() => {
+    fetch("https://www.mocky.io/v2/5d531c4f2e0000620081ddce") 
+      .then((data) => data.json())  // Retorna um dado qualquer e transforma em json
+      .then((result) => { // Retornando o resultado
+        // console.log(result);
+        setPersons(result);
+      })
+  },[])
 
   // Função que recebe o objeto no evento click
-  const handleClick = (objeto) => {
-    // alert(JSON.stringify(objeto.name));
-    // Irá abrir o modal ao ser clicado
-    setMostraModal("block");
+  const handleClick = (result) => {
+    if (selectPerson.id === result.id) {
+
+      setViewModal();  // Fechar o modal clicando de novo no mesmo botão do user, mas passando "none" no parêntese
+    } else {
+
+      setViewModal("block");  // Se o botão clicado não seja o mesmo ele mostra o nome do user clicado
+      setSelectPerson(result);  // Irá mostrar a pessoa selecionada
+    } 
   }
-      
+
+  // Mandado informação do input e do objeto para a API, Obs. mas também mandando o parâmetro capturado no input
+  const handleSearch = (e) => {
+    e.preventDefault();
+    let body = JSON.stringify({
+      // Card Info
+      card_number: cards[searchParam2].card_number,
+      cvv: cards[searchParam2].cvv,
+      expiry_date: cards[searchParam2].expiry_date,
+
+      // Destination User ID
+      destination_user_id: selectPerson.id,
+
+      // Value of the Transaction
+      value: searchParam1
+    });
+
+    fetch("https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989", {
+      method: "POST",
+      body: body
+    })
+    .then((data) => data.json())
+    .then((result) => { // Retornando o resultado
+      console.log(result);
+      if (result.success === true) {
+        setViewModal("none");
+        setSearchParam1("");
+        setSearchParam2("");
+        
+        alert("Aprovadro!");
+      } else {
+
+        alert("Não foi Aprovadro!");
+      }      
+    }).catch(err => console.log(err));
+  }
+
+  const setFecharModal = (e) => {
+    e.preventDefault();
+    setViewModal("none");
+  }
+
   return (
     <>
-      { persons.map(objeto => {
+      { persons.map(result => {
         return <ul>
           <div className="container">
             <div className="picture">
-              <img src={objeto.img} alt=""/>
+              <img src={result.img} alt=""/>
             </div>
             <div>
-              <li> Nome do Usuário: {objeto.name} </li>
-              <li> ID: {objeto.id} </li>
-              <li> Username: {objeto.username} </li>
+              <li> Nome do Usuário: {result.name} </li>
+              <li> ID: {result.id} </li>
+              <li> Username: {result.username} </li>
             </div>
             <div>
-              {/* Função feita aqui inline mas só que ela irá retornar o objeto que está sendo capturado via click */}
-              <button onClick={(event) => {handleClick(objeto)}}>Pagar</button>
+              {/* Função inline mas só que irá retornar o objeto que está sendo capturado via click */}
+              <button onClick={() => {handleClick(result)}}>Pagar</button>
             </div>          
-          </div>                                                                                           
-        </ul>
+          </div>
+          </ul>
         })
       }
 
       {/* Chama a variável que foi definida na state que determina se irá abrir o modal */}
-      <div className="modal" style={{display:mostraModal}}>
+      <div className="modal" style={{display:viewModal}}>
        <div className="modalInterna">
-         <form action="#" method="post">
+         <form onSubmit={handleSearch}>
            <div className="cabecalho">
-             <h4>Pagamento para  </h4>
+             <h4>Pagamento para <span className="nameUser">{selectPerson.name}</span></h4>
            </div>
            <div>
-             <input min="0.01" step="0.01" type="number" placeholder="R$ 0,00" />
+             {/* Função que cuida da entrada do user, aqui chamamos a variável que está criada lá em cima, Obs. aqui mandamos o evento com o target pegando o valor com o value */}
+             <input onChange={(e) => { setSearchParam1(e.target.value) }} min="0.01" step="0.01" type="number" placeholder="R$ 0,00" value={searchParam1} />
            </div>
            <div>
-             <select name="cards" id="cards">
-               <option value="card1" selected>Selecione um cartão</option>
-               <option value="card2">1111</option>
-               <option value="card3">1234</option>                 
+             <select onChange ={(e) => { setSearchParam2(e.target.value) }} requerid>
+              <option value="" selected>Selecione um cartão</option>
+              {
+                cards.map((cartao, i) => {
+                  return <option value={i}>{cartao.card_number.substring(12)}</option>
+                })
+              }
              </select>
            </div>
            <div>
+             {/* Chamando a função que irá fazer a busca do pagamento */}
              <button type="submit">Pagar</button>
-             <button id="botaoVoltar">Voltar</button>
+             <button onClick ={setFecharModal} className="buttonClosed">Fechar</button>
            </div>
          </form>
        </div>
